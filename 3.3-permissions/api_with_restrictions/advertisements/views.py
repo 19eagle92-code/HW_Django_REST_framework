863621f2f7cac71b6e7e7e6a7ef599fadeaf7b32
Kirill Gmirya <Gmiryk_Kirill@mail.ru>
2026-03-09 20:15:37 +0300

    Update views.py

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,5 +1,12 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import status
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -8,8 +15,25 @@ class AdvertisementViewSet(ModelViewSet):
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
+    queryset = Advertisement.objects.all().order_by("-created_at")
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
+        if self.action in ["create", "update", "partial_update", "destroy"]:
             return [IsAuthenticated()]
         return []
+
+    def destroy(self, request, *args, **kwargs):
+        """Удалять объявление может только его автор."""
+        instance = self.get_object()
+
+        if instance.creator != request.user:
+            return Response(
+                {"detail": "Вы не можете удалять чужое объявление"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        return super().destroy(request, *args, **kwargs)
